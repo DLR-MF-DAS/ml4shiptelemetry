afd07adb2528c19125a26a3142e8522668904442
Rikard Vinge <rikard.vinge@dlr.de>
2024-10-24 15:11:42 +0200

    Add time series cross validation

--- src/ml4shiptelemetry/RFRegressorModel.py
+++ src/ml4shiptelemetry/RFRegressorModel.py
@@ -1,36 +1,67 @@
-import numpy as np
-import matplotlib.pyplot as plt
 from sklearn.ensemble import RandomForestRegressor
-from sklearn.metrics import r2_score
-from sklearn.model_selection import KFold
+from sklearn.metrics import r2_score, mean_absolute_error
+from sklearn.model_selection import KFold, TimeSeriesSplit
+from typing import List
+import numpy as np
 
 
 class RFRegressor:
     
     regressor = None
 
-    def __init__(self, num_trees, maximum_depth, seed = 18):
+    def __init__(self, num_trees, maximum_depth, seed = 18, target_names: List[str] = None, scores=None):
         self.num_trees = num_trees
         self.maximum_depth = maximum_depth
         self.seed = seed
         self.regressor = RandomForestRegressor(n_estimators = self.num_trees, max_features = 'sqrt', max_depth = self.maximum_depth, random_state = self.seed)
+        self.target_names = target_names
+        if scores is not None:
+            self.scores = scores
+        else:
+            self.scores = {'R2': r2_score, 'MAE': mean_absolute_error}
+
 
     def train(self, x, y):
         self.regressor.fit(x, y)
 
-    def crossvalidate(self, x, y, num_splits = 5):
-        kf = KFold(n_splits=num_splits, shuffle=True)
-        iteration = 1
-        for train, test in kf.split(x):
+
+    def crossvalidate(self, x, y, num_splits = 5, time_series=False, shuffle=True):
+        if time_series:
+            kf = TimeSeriesSplit(n_splits=num_splits)
+        else:
+            kf = KFold(n_splits=num_splits, shuffle=shuffle)
+
+        for iteration, (train, test) in enumerate(kf.split(x)):
             regressor = RandomForestRegressor(n_estimators = self.num_trees, max_features = 'sqrt', max_depth = self.maximum_depth, random_state = self.seed).fit(x[train], y[train])
-            prediction = regressor.predict(x[train])
-            print(f"Iteration {iteration} - Training R2 Score:")
-            print(r2_score(y[train], prediction, multioutput='raw_values'))
-            prediction = regressor.predict(x[test])
-            print(f"Iteration {iteration} - Test R2 Score:")
-            print(r2_score(y[test], prediction, multioutput='raw_values'))
+            prediction_train = regressor.predict(x[train])
+            prediction_test = regressor.predict(x[test])
+
+            # Print scores
+            train_scores = []
+            test_scores = []
+            for metric in self.scores.values():
+                train_scores.append(metric(y[train], prediction_train, multioutput='raw_values'))
+                test_scores.append(metric(y[test], prediction_test, multioutput='raw_values'))
+            self.print_performance( title=f"Iteration {iteration} - Training Score:", scores=train_scores, score_names=self.scores.keys(), num_decimals=4)
+            self.print_performance( title=f"Iteration {iteration} - Test Score:", scores=test_scores, score_names=self.scores.keys(), num_decimals=4)
             print("----------------")
-            iteration += 1
+
 
     def predict(self, x):
         return self.regressor.predict(x)
+    
+
+    def print_performance(self, title, scores: List[float], score_names: List[str], num_decimals: int = 4):
+        
+        print(title)
+        if self.target_names is None:
+            for score, score_name in zip(scores, score_names):
+                print(f'{score_name}: {", ".join([f"{s:.{num_decimals}f}" for s in score])}')
+        else:
+            max_length = max(max([len(s) for s in self.target_names]), num_decimals+2) + 2
+            metric_strs = [[f'{s:.{num_decimals}f}' for s in score] for score in scores]
+            length_str = f':>{max_length}'
+            row_format =f"{{{length_str}}}" * (len(self.target_names) + 1)
+            print(row_format.format("", *self.target_names))
+            for metric, row in zip(score_names, metric_strs):
+                print(row_format.format(metric, *row))  
--- src/ml4shiptelemetry/__main__.py
+++ src/ml4shiptelemetry/__main__.py
@@ -3,12 +3,15 @@ from .RFRegressorModel import RFRegressor
 from .dataloader import process_files
 
 @click.command()
-@click.option('--data-dir', help='Data directory contaning .tab files and folders with .dat files', required=False)
-def main(data_dir):
-    rfregressor = RFRegressor(100, 18)
-    #data_dir = '/localhome/zapp_an/Desktop/Vouchers/geomar_voucher/data/'
-    x, y = process_files(data_dir)
-    rfregressor.crossvalidate(x, y, num_splits = 5)
+@click.option('--data-dir', help='Data directory contaning .tab files and folders with .dat files', required=True)
+@click.option('--ts_cv', is_flag=True, help='Use time series cross validation instead of regular cross validation', default=False, type=bool)
+def main(data_dir, ts_cv):
+    # Load data
+    x, y, target_names = process_files(data_dir, sort_time=ts_cv)
+    
+    # Create regressor
+    rfregressor = RFRegressor(100, 18, target_names=target_names)
+    rfregressor.crossvalidate(x, y, num_splits = 5, time_series=ts_cv)
 
 if __name__ == "__main__":
     main()
